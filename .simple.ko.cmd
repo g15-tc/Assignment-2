8ba7abc7d45bbd3305dfd1704c8d4c1dd2e6af2a
Test <test@test-VirtualBox.(none)>
2013-10-03 14:41:00 -0700

    Tommy's First Commit

---
+++ .simple.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/test/work/eece315/assign2/simple.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.8.0-31-generic/scripts/module-common.lds --build-id  -o /home/test/work/eece315/assign2/simple.ko /home/test/work/eece315/assign2/simple.o /home/test/work/eece315/assign2/simple.mod.o
---
+++ .tmp_versions/simple.mod
@@ -0,0 +1,2 @@
+/home/test/work/eece315/assign2/simple.ko
+/home/test/work/eece315/assign2/simple.o
